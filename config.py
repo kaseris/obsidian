b4881685407d56257804fa0f59870e76af04213f
kaseris <mich.kaseris@gmail.com>
2023-03-25 21:18:45 +0200

    Service account key for GCP access.

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
+GCS_SERVICE_ACCOUNT_KEY = 'concise-isotope-381417-76b805e29be0.json'
+
 DEEP_FASHION_LOW_RES_URL = 'https://storage.googleapis.com/deep-fashion-bucket/img.zip'
 DEEP_FASHION_HI_RES_URL = None
