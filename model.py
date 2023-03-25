80e21442ff82087796225c22648125bfb3dd068f
kaseris <mich.kaseris@gmail.com>
2023-03-25 21:50:06 +0200

    Documentation

--- model.py
+++ model.py
@@ -32,10 +32,40 @@ def linear_cls_head(fan_in, embedding_sz, num_classes):
 
 @MODELS.register('ResNetDeepFashion')
 class ResNetDeepFashion(nn.Module):
+    """
+    A class that defines the ResNetDeepFashion model.
+
+    The ResNetDeepFashion model is a modified version of the ResNet architecture
+    that is optimized for the DeepFashion dataset. The model consists of a ResNet
+    backbone and a classification head that is used to predict the class labels of
+    the input images.
+
+    Attributes:
+    ----------
+    backbone : str
+        The backbone architecture to use, e.g. "resnet50".
+    cls_head_type : str
+        The type of classification head to use, e.g. "linear".
+    embedding_sz : int
+        The size of the output embeddings.
+        
+    """
     def __init__(self,
                  backbone: str,
                  cls_head_type: str,
                  embedding_sz: int):
+        """
+        Initialize the ResNetDeepFashion model.
+
+        Args:
+        ----------
+        backbone : str
+            The backbone architecture to use, e.g. "resnet50".
+        cls_head_type : str
+            The type of classification head to use, e.g. "linear".
+        embedding_sz : int
+            The size of the output embeddings.
+        """
         super(ResNetDeepFashion, self).__init__()
         self.resnet = BACKBONES[backbone]()
         self.cls_head_type = cls_head_type
@@ -43,6 +73,9 @@ class ResNetDeepFashion(nn.Module):
         self._prepare_model()
 
     def _prepare_model(self):
+        """
+        Prepare the ResNetDeepFashion model by modifying the final layer.
+        """
         num_features = self.resnet.fc.in_features
         self.resnet.fc = nn.Sequential(*CLS_HEADS[self.cls_head_type](num_features,
                                                                       self.embedding_sz,
