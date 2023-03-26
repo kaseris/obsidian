dda1f0463e520c6151df273fb9fab25b6e041835
kaseris <mich.kaseris@gmail.com>
2023-03-26 22:44:48 +0300

    getitem implementation

--- dataset.py
+++ dataset.py
@@ -1,18 +1,50 @@
 import os
 import os.path as osp
-from os import PathLike
+
+from typing import Dict
 
 import torch
+import torch.nn.functional as F
+from PIL import Image
 from torch.utils.data import Dataset
-
-from typing import Union
+from torchvision import transforms
 
 import config
 from registry import Registry
 
 DATASETS = Registry()
+TRANSFORMS = Registry()
 
+@TRANSFORMS.register('DeepFashion_default')
+def deepfashion_default_transform():
+    """
+    Returns a set of image transforms commonly used for training DeepFashion datasets.
 
+    The transforms include:
+    - Resizing the image to the size specified in the `config` module.
+    - Randomly cropping a portion of the image to the size specified in the `config` module.
+    - Randomly flipping the image horizontally.
+    - Converting the image to a PyTorch tensor.
+    - Normalizing the image using the mean and standard deviation values provided in the torchvision documentation.
+
+    Returns:
+    - A `transforms.Compose` object that can be passed to a PyTorch `DataLoader` object.
+
+    Example:
+    ```
+    transform = deepfashion_default_transform()
+    dataset = DeepFashionDataset('path/to/data', transform=transform)
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+    ```
+    """
+    return transforms.Compose([
+        transforms.Resize(config.IMG_SIZE),
+        transforms.RandomCrop(config.CROP_SIZE),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    
 @DATASETS.register('deepfashion_cat_att')
 class DeepFashionCategoryAttribute(Dataset):
     """
@@ -36,12 +68,33 @@ class DeepFashionCategoryAttribute(Dataset):
         __getitem__(index: int) -> Dict[str, Union[str, int, List[int]]]: Returns a dictionary containing
             information about the image at the given index. The dictionary contains the keys "path", "cat_index",
             "category", and "attributes" (if include_attributes is True).
+    Example:
+    ```
+    from dataset import DeepFashionCategoryAttribute, TRANSFORMS
 
+    # create an instance of the dataset
+    dataset = DeepFashionCategoryAttribute(include_attributes=True,
+                                        transforms=TRANSFORMS['DeepFashion_default']())
+
+    # get information about the first image in the dataset
+    sample = dataset[0]
+
+    # print the shape of the transformed image tensor and attribute tensor
+    print(sample['img'].shape)
+    print(sample['attributes'].shape)
+    ```
     """
 
     def __init__(self,
-                 include_attributes=False):
+                 include_attributes=False,
+                 transforms=None):
         self.data_dir = config.DEEP_FASHION_DIR
+        self.transforms = transforms
+        self.include_attributes = include_attributes
+        
+        if self.transforms is None:
+            self.transforms = TRANSFORMS['DeepFashion_default']()
+        
         self.CLASS_LABELS = []
         self.ATTR_LABELS = []
 
@@ -71,15 +124,31 @@ class DeepFashionCategoryAttribute(Dataset):
                         "cat_index": int(line.split()[1]),
                         "category": self.IDX_TO_CLASS[int(line.split()[1])]
                     }
+        with open(config.DEEP_FASHION_CLOTHING_LIST_BBOX_IMG_PATH, 'r') as f:
+            for idx, line in enumerate(f):
+                if idx > 1:
+                    self.DATA_DICT[idx-2]['bbox'] = line.strip().split(' ')[-4:]
+        
         if include_attributes:
             with open(config.DEEP_FASHION_CLOTHING_LIST_ATT_IMG_PATH, 'r') as f:
                 for idx, line in enumerate(f):
                     if idx > 1:
-                        self.DATA_DICT[idx-2]['attributes'] = [idx for idx,
-                                                        att in enumerate(line.split()[1:]) if int(att) > 0]
+                        self.DATA_DICT[idx-2]['attributes'] = line.strip().split(' ')[-1000:]
 
     def __len__(self):
         return len(list(self.DATA_DICT.keys()))
 
-    def __getitem__(self, index):
-        return self.DATA_DICT[index]
+    def __getitem__(self, index) -> Dict[torch.TensorType,
+                                         torch.TensorType]:
+        img = Image.open(osp.join(self.data_dir, self.DATA_DICT[index]['path'])).convert('RGB')
+        crop = list(map(int, self.DATA_DICT[index]['bbox']))
+        img_cropped = img.crop(crop)
+        img_transformed = self.transforms(img_cropped)
+        return_dict = {'img': img_transformed,
+                       'category': F.one_hot(torch.tensor(self.DATA_DICT[index]['cat_index'] - 1), num_classes=len(self.CLASS_LABELS))}
+        if self.include_attributes:
+            attributes = list(map(int, [el if el != '' else '-1' for el in self.DATA_DICT[index]['attributes']]))
+            attributes = torch.clamp(torch.tensor(attributes), 0, 1)
+            return_dict['attributes'] = attributes        
+        return return_dict
+    
