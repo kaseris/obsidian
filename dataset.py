e4bf87af213d538e2192ae4462934ae73ac22925
kaseris <mich.kaseris@gmail.com>
2023-03-30 14:15:40 +0300

    Got rid of the intensive annotation reading

--- dataset.py
+++ dataset.py
@@ -86,11 +86,16 @@ class DeepFashionCategoryAttribute(Dataset):
     """
 
     def __init__(self,
+                 data_dir=config.DEEP_FASHION_DIR,
                  include_attributes=False,
-                 transforms=None):
-        self.data_dir = config.DEEP_FASHION_DIR
+                 transforms=None,
+                 split_type='train',
+                 split_info=None,
+                 garment_annotations=None):
+        self.data_dir = data_dir
         self.transforms = transforms
         self.include_attributes = include_attributes
+        self.split_type = split_type
         
         if self.transforms is None:
             self.transforms = TRANSFORMS['DeepFashion_default']()
@@ -113,41 +118,25 @@ class DeepFashionCategoryAttribute(Dataset):
 
 
         self.CLS_TO_IDX = {v: k for k, v in self.IDX_TO_CLASS.items()}
-
-        self.DATA_DICT = dict()
-
-        with open(config.DEEP_FASHION_CLOTHING_LIST_CAT_IMG_PATH, 'r') as f:
-            for idx, line in enumerate(f):
-                if idx > 1:
-                    self.DATA_DICT[idx-2] = {
-                        "path": line.split()[0],
-                        "cat_index": int(line.split()[1]),
-                        "category": self.IDX_TO_CLASS[int(line.split()[1])]
-                    }
-        with open(config.DEEP_FASHION_CLOTHING_LIST_BBOX_IMG_PATH, 'r') as f:
-            for idx, line in enumerate(f):
-                if idx > 1:
-                    self.DATA_DICT[idx-2]['bbox'] = line.strip().split(' ')[-4:]
         
-        if include_attributes:
-            with open(config.DEEP_FASHION_CLOTHING_LIST_ATT_IMG_PATH, 'r') as f:
-                for idx, line in enumerate(f):
-                    if idx > 1:
-                        self.DATA_DICT[idx-2]['attributes'] = line.strip().split(' ')[-1000:]
+        self.split = split_info[self.split_type]
+        self.annos = garment_annotations
 
     def __len__(self):
-        return len(list(self.DATA_DICT.keys()))
+        return len(self.split)
 
     def __getitem__(self, index) -> Dict[torch.TensorType,
                                          torch.TensorType]:
-        img = Image.open(osp.join(self.data_dir, self.DATA_DICT[index]['path'])).convert('RGB')
-        crop = list(map(int, self.DATA_DICT[index]['bbox']))
+        pth = self.split[index]
+        
+        img = Image.open(osp.join(self.data_dir, pth)).convert('RGB')
+        crop = self.annos[pth]['bbox']
         img_cropped = img.crop(crop)
         img_transformed = self.transforms(img_cropped)
         return_dict = {'img': img_transformed,
-                       'category': torch.tensor(self.DATA_DICT[index]['category'], dtype=torch.float)}
+                       'category': torch.LongTensor(self.annos[pth]['category'])}
         if self.include_attributes:
-            attributes = list(map(int, [el if el != '' else '-1' for el in self.DATA_DICT[index]['attributes']]))
+            attributes = self.annos[pth]['attributes']
             attributes = torch.clamp(torch.tensor(attributes), 0, 1)
             return_dict['attributes'] = attributes        
         return return_dict
