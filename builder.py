39e5d6dced98e4fc0bbf578aba0223de121cfaf6
kaseris <mich.kaseris@gmail.com>
2023-04-06 16:43:14 +0300

    Model build API

---
+++ builder.py
@@ -0,0 +1,42 @@
+import os.path as osp
+import json
+
+from model import MODELS
+
+
+def build_model(cfg):
+    """
+    Builds a model from a dictionary read from a JSON file.
+
+    Args:
+        cfg (dict): A dictionary containing the model configuration.
+
+    Returns:
+        nn.Module: A PyTorch model instantiated from the configuration.
+
+    Raises:
+        KeyError: If the configuration dictionary does not have a 'type' key.
+        ValueError: If the 'type' key in the configuration does not correspond
+            to a registered model type in the MODELS registry.
+
+    Example:
+    >>> with open('configs/base_model.json', 'r') as f:
+    >>>     data = json.load(f)
+    >>> model = build_model(data['model'])
+    >>> print(model)
+    """
+    model_cfg = cfg['model_cfg']
+    model_type = cfg['type']
+
+    if model_type not in MODELS.registry.keys():
+        raise ValueError(f'Unsupported model type: `{model_type}`. '
+                         f'Must be one of \n{list(MODELS.registry.keys())}')
+        
+    return MODELS[model_type](**model_cfg)
+        
+if __name__ == '__main__':
+    with open(osp.join('configs', 'base_model.json'), 'r') as f:
+        data = json.load(f)
+    model = build_model(data['model'])
+    print(model)
+    
