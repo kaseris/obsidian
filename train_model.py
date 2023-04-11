d37d769e659b1af82f7a01981bc7ffaeb6ed7ec0
kaseris <mich.kaseris@gmail.com>
2023-04-11 19:01:56 +0300

    Set env variable

--- train_model.py
+++ train_model.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 
 from builder import build_trainer
@@ -8,6 +9,7 @@ args.add_argument('--wandb_api_key', type=str, required=True)
 args.add_argument('--config', type=str, default='configs/resnet_cgd_base.json', required=True)
 
 def main():
+    os.environ['WANDB_API_KEY'] = args.wandb_api_key
     trainer = build_trainer(args.config)
     trainer.train()
     
