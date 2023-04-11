3840853cc1746b4960902ad35a8252091b6fa6e8
kaseris <mich.kaseris@gmail.com>
2023-04-11 19:41:29 +0300

    Argument parser

--- train_model.py
+++ train_model.py
@@ -4,19 +4,19 @@ import logging
 
 from builder import build_trainer
 
-args = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description='Train interface')
 
-args.add_argument('--wandb_api_key', type=str, required=True)
-args.add_argument('--config', type=str, default='configs/resnet_cgd_base.json', required=True)
-args.parse_args()
+
+parser.add_argument('--wandb_api_key', type=str, required=True)
+parser.add_argument('--config', type=str,
+                  default='configs/resnet_cgd_base.json', required=True)
+args = parser.parse_args()
 
 def main():
     os.environ['WANDB_API_KEY'] = args.wandb_api_key
     logging.basicConfig(level=logging.DEBUG)
     trainer = build_trainer(args.config)
     trainer.train()
-    
 
 if __name__ == '__main__':
     main()
-    
