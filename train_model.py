f6f460e621db3a6b461bc0ce380a9307ead73acf
kaseris <mich.kaseris@gmail.com>
2023-04-12 14:01:45 +0300

    Setting the env variables right

--- train_model.py
+++ train_model.py
@@ -2,21 +2,23 @@ import os
 import argparse
 import logging
 
-from builder import build_trainer
-
 parser = argparse.ArgumentParser(description='Train interface')
 
 
 parser.add_argument('--wandb_api_key', type=str, required=True)
 parser.add_argument('--config', type=str,
                   default='configs/resnet_cgd_base.json', required=True)
+parser.add_argument('--dataset_path', type=str, required=True)
 args = parser.parse_args()
 
+
 def main():
-    os.environ['WANDB_API_KEY'] = args.wandb_api_key
     logging.basicConfig(level=logging.DEBUG)
     trainer = build_trainer(args.config)
     trainer.train()
 
 if __name__ == '__main__':
+    os.environ['DATASET_DIR'] = args.dataset_path
+    os.environ['WANDB_API_KEY'] = args.wandb_api_key
+    from builder import build_trainer
     main()
