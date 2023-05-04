2b87461ee92d1e93a2230cd7cd6887345a98745b
kaseris <mich.kaseris@gmail.com>
2023-05-04 16:05:49 +0300

    Add tests.

---
+++ tests/test_imports.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+def test_imports():
+    import obsidian
+    import obsidian.blocks
+    import obsidian.coco
+    import obsidian.core
+    import obsidian.blocks.classification
+    import obsidian.blocks.detection
+    import obsidian.blocks.losses
+    import obsidian.blocks.pooling
+    import obsidian.datasets
+    import obsidian.datasets.dataset
+    import obsidian.datasets.transforms
+    assert True
