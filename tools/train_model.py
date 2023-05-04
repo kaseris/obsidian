d11035bc8c145d0fe9063f8e6cda4ed4458ee7ac
kaseris <mich.kaseris@gmail.com>
2023-05-04 14:10:04 +0300

    Moved the training of various models and configs to tools. Everything must be imported.

---
+++ tools/train_model.py
@@ -0,0 +1,25 @@
+import os
+import argparse
+import logging
+
+parser = argparse.ArgumentParser(description='Train interface')
+
+
+parser.add_argument('--wandb_api_key', type=str, required=True)
+parser.add_argument('--config', type=str,
+                    default='configs/detection/base.yaml', required=True)
+parser.add_argument('--dataset_path', type=str, required=True)
+args = parser.parse_args()
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    trainer = build_trainer(args.config)
+    trainer.train()
+
+
+if __name__ == '__main__':
+    os.environ['DATASET_DIR'] = args.dataset_path
+    os.environ['WANDB_API_KEY'] = args.wandb_api_key
+    from builder import build_trainer
+    main()
