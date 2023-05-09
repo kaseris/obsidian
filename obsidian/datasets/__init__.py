b26ad2b8a1f5b6c61f7e1221ec87289c85f0a098
kaseris <mich.kaseris@gmail.com>
2023-05-09 16:42:57 +0300

    Datasets module.

--- obsidian/datasets/__init__.py
+++ obsidian/datasets/__init__.py
@@ -1,10 +1,4 @@
-from .dataset import (DeepFashionCategoryAttribute, FashionIQ, DeepFashion2)
-from .transforms import (deepfashion_object_detection_default_transform,
-                         deepfashion_default_transform,
-                         deepfashion_validation_transform)
+from . import dataset
+from . import transforms
+
 from .registry_ import TRANSFORMS, DATASETS
-
-
-__all__ = ['deepfashion_object_detection_default_transform',
-           'DeepFashionCategoryAttribute', 'FashionIQ', 'deepfashion_default_transform',
-           'deepfashion_validation_transform', 'DeepFashion2', 'DATASETS', 'TRANSFORMS']
